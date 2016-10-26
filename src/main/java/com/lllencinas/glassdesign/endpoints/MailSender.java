b2ed32c9bda8ddfe8f8c527074584d760935a5a8
Lucas Lencinas <lllencinas@gmail.com>
2016-10-26 00:44:45 -0300

    Working with mailgun

--- src/main/java/com/lllencinas/glassdesign/endpoints/MailSender.java
+++ src/main/java/com/lllencinas/glassdesign/endpoints/MailSender.java
@@ -1,23 +1,18 @@
 package com.lllencinas.glassdesign.endpoints;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-
-
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 @Path("/mailsender")
 public class MailSender {
@@ -27,32 +22,20 @@ public class MailSender {
 	public Response echoMessage(){
 		System.out.println("Me llego un GET a echo");
 
-		// ...
-		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props, null);
-		String msgBody = "Body del mensaje de ejemplo";
+		String MAILGUN_DOMAIN_NAME = "mg.glassdesign";
+		String MAILGUN_API_KEY = "key-5bd724f94fa38e82a0a602ff3fcdb2dd";
+		Client client = Client.create();
+		client.addFilter(new HTTPBasicAuthFilter("api", MAILGUN_API_KEY));
+		WebResource webResource = client.resource("https://api.mailgun.net/v3/" + MAILGUN_DOMAIN_NAME
+				+ "/messages");
+		MultivaluedMapImpl formData = new MultivaluedMapImpl();
+		formData.add("from", "Glass Design <lucaslencinas@" + MAILGUN_DOMAIN_NAME + ">");
+		formData.add("to", "lllencinas@gmail.com");
+		formData.add("subject", "Simple Mailgun Example");
+		formData.add("text", "Plaintext content");
+		webResource.type(MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class, formData);
 
-		try {
-		    Message msg = new MimeMessage(session);
-		    msg.setFrom(new InternetAddress("lucas.lencinas@hotmail.com", "Mister Lucas"));
-		    msg.addRecipient(Message.RecipientType.TO,
-		     new InternetAddress("lllencinas@gmail.com", "glassdesign admin"));
-		    msg.setSubject("Subject de ejemplo");
-		    msg.setText(msgBody);
-		    Transport.send(msg);
-
-		} catch (AddressException e) {
-		    // ...
-			System.out.println(e.getMessage());
-		} catch (MessagingException e) {
-		    // ...
-			System.out.println(e.getMessage());
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		
 		return Response.ok("{}",MediaType.APPLICATION_JSON).build();
 	}
 }
+
